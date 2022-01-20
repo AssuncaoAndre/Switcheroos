7526c3afc336a35eaaf341755bf2cb16e98c430a
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-20 13:42:11 +0000

    Products for the database

--- backend/populate.sql
+++ backend/populate.sql
@@ -1,3 +1,12 @@
-INSERT INTO Product VALUES(1,"Switch","Black T-Shirt",              20,"Small","Woman", "T-shirts",  "This is it"  ,"themes/images/cloth/1.jpg",   "Black, Red",   10,15);
-INSERT INTO Product VALUES(2,"Switch","Slightly less black T-shirt",30,"Small","Man",   "Pants",     "This is it"     ,"themes/images/cloth/2.jpg",   "Black, Yellow",10,15);
-INSERT INTO Product VALUES(3,"Switch","Another Product",            70,"Large","Man",   "T-shirts",    "This is it 99" ,"themes/images/cloth/2.jpg",   "Black",        10,15);
+--coda, bran, nam, pri, siz, sx, atgory, sription, imag, olor, quantity, arbon_footprint
+
+INSERT INTO Product VALUES(1,"Generic","Black T-Shirt",              10,"Small","Men", "T-Shirts",  "A simple black t-shirt."  ,"themes/images/cloth/Black-T-Shirt.jpg",   "Black",   10,15);
+INSERT INTO Product VALUES(2,"Levi's","Grey Hoodie",30,"Small","Men",   "Hoodies",     "Grey Hoodie provided by Levi's."     ,"themes/images/cloth/grey-hoodie.jpg",   "Grey",8,15);
+INSERT INTO Product VALUES(3,"Generic","Black Pants",            70,"Medium","Woman",   "Pants",    "The new female fashion." ,"themes/images/cloth/black-trousers.jpg",   "Black",        10,40);
+INSERT INTO Product VALUES(4,"Generic", "Blue Jeans",20,"Medium","Men","Pants","Simple pants.","themes/images/cloth/blue-jeans.jpg","Blue",10,20);
+INSERT INTO Product VALUES(5,"Nike", "Air force 1",60,"42","Men","Shoes","The all time favorite by Nike. In 2 colors","themes/images/cloth/air-force-1.jpeg","White and black",7,50);
+INSERT INTO Product VALUES(6,"Eco", "Black Eco Shoes",90,"39","Men","Shoes","For the better our planet","themes/images/cloth/ecological-shoes.jpg","Blue",4,20);
+INSERT INTO Product VALUES(7,"Eco", "Keep It Green Hat",15,"One Size Fits All","Both","Hats","Make the world a better place","themes/images/cloth/keep-it-green-hat.jpg","Brown",10,20);
+INSERT INTO Product VALUES(8,"Eco", "Eco Bag",5,"Medium","Both","Bags","No more plastic.","themes/images/cloth/eco-bag.jpg","Tan",10,0);
+INSERT INTO Product VALUES(9,"Generic", "Electric blue shorts",30,"Medium","Men","Shorts","Your summer favorite.","themes/images/cloth/electric-blue-shorts-casual.jpg","Blue",5,30);
+INSERT INTO Product VALUES(10,"Vans", "Vans Old Skool",50,"38","Both","Pants","The oldies are back. Get ready for your summer","themes/images/cloth/vans-shoes","Black",2,80);
